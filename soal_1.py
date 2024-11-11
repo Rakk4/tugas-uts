f63818da19be25a0675305a87c0aa7d7859436ea
unknown <2403010056@unper.ac.id>
2024-11-11 10:39:08 +0700

    perubahan kedua

---
+++ soal_1.py
@@ -0,0 +1,8 @@
+
+
+panjang=float(input("masukan panjang"))
+Lebar=float(input("masukan lebar"))
+Luas= panjang*Lebar
+Keliling= 2*(panjang+Lebar)
+print(f'luas dari{panjang}*{Lebar}={Luas}')
+print(f'Keiling dari{panjang}dan{Luas}adalah{Keliling}')
---
+++ soal_3.py
@@ -0,0 +1,5 @@
+kata_pertama = str(input("Masukan kata pertama"))
+kata_kedua = str(input("Masukan kata kedua"))
+gabungkan_kata = kata_pertama + kata_kedua
+spasi = gabungkan_kata.replace(" ", "")
+print("kata yang anda masukan",spasi)
